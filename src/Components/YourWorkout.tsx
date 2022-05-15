c72fa81fe1c489317f27524da8f1ead40a26ee09
Felix J <felix.johnsson@sneakersnstuff.com>
2022-05-15 14:24:03 +0200

    Saving new order from Your Workouts

--- src/Components/YourWorkout.tsx
+++ src/Components/YourWorkout.tsx
@@ -5,14 +5,34 @@ import '../Styling/YourWorkout.css'
 import { DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
 import { useState } from "react"
 
-export const YourWorkout = (props:any) => {
+export const YourWorkout = (props:any) => { // @TODO - add type to props
+	const serverURL = 'http://localhost:5000'
 	const [workoutOrder, setWorkoutOrder] = useState(props.userData.workouts)
+
+	const saveNewOrder = (items: any) => {
+		fetch(`${serverURL}/save_workouts`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				username: props.userData.username,
+				workouts: items,
+			})
+		})
+		.then(res => res.json())
+		.then((data) => {
+			console.log(data)
+		})
+		.catch(err => console.log(err))
+	}
 	
 	const onDragEnd = (result:any) => {
 		const items = Array.from(workoutOrder)
 		const [reorderitem] = items.splice(result.source.index, 1)
 		items.splice(result.destination.index, 0, reorderitem)
 		setWorkoutOrder(items)
+		saveNewOrder(items)
 	}
 
 	return (
