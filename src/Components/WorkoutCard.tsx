baf8859c6d88cac87005138bc9c22e9cae4c4732
Felix J <felix.johnsson@sneakersnstuff.com>
2022-05-15 15:43:24 +0200

    Small workout card update

--- src/Components/WorkoutCard.tsx
+++ src/Components/WorkoutCard.tsx
@@ -1,12 +1,18 @@
-import Card from '@mui/material/Card'
+import React from 'react'
 
 export const WorkoutCard = (props:any) => {
 	return (
-		<Card
-			className={`your-workout-card ${props.workout.category[0].replace(' ', '-')}`}>
-			<h2>{props.workout.name}</h2>
-			<p>{props.workout.category}</p>
-			<p>{props.workout.description}</p>
-		</Card>
+		<div className={`your-workout-card ${props.workout.category[0].replace(' ', '-')}`}>
+			<div className='title-container'>
+				<h2>{props.workout.name}</h2>
+				<p>{props.workout.category}</p>
+			</div>
+			<div className='description-container'>
+				<p>{props.workout.description}</p>
+			</div>
+			<div className='notes-container'>
+				<p>{props.workout.notes}</p>
+			</div>
+		</div>
 	)
 }
